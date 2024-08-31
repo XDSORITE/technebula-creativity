4e14cb000ac19d89d8345872310ac02b0069e270
XDSORTIE <xdsorite@gmail.com>
2024-08-31 15:55:04 +0400

    summary

--- framerusercontent.com/sites/7f6QTocP9i0wHZE7ihbvWV/chunk-LTP5OET2.mjs
+++
@@ -1,2 +0,0 @@
-function t(a,e){return{favicon:"https://framerusercontent.com/assets/0rwis00lgc2m9kuBmNEZjbMTvE.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/i7Is5t6qUcUVN8Kgvi9xTwlh8.jpg",title:"TechNebula creativity"}}export{t as a};
-//# sourceMappingURL=chunk-LTP5OET2.mjs.map
